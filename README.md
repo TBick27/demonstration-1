04650370f91856602136c044442937516b890bf9
Tristan Bickel <tbickel@academic.rrc.ca>
2025-01-09 15:47:13 -0600

    added more functionality to display my handsome name plus in the read me

---
+++ README.md
@@ -0,0 +1,7 @@
+# Module 01 Demonstration
+## Description
+Hello World Application
+says how handsome i am
+
+## Author
+Tristan Bickel
---
+++ hello_world.py
@@ -0,0 +1,8 @@
+"""
+Description: Hello World display Program
+Author: Tristan Bickel
+Date: 2025-01-09
+"""
+
+print("Hello World!! I am Handsome!!")
+print("Tristan Bickel IS BEAUTIFUL!!!")
