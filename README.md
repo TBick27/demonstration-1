9f130736e1708957dffc16ee0263f244933d0704
Tristan Bickel <tbickel@academic.rrc.ca>
2025-01-13 10:26:18 -0600

    the final onee

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Module 01 Demonstration
 ## Description
 Hello World Application
+has multiple languages!
 says how handsome i am
 told michael i refuse to add french to my project
 
--- hello_world.py
+++ hello_world.py
@@ -7,3 +7,5 @@ Date: 2025-01-09
 print("Hello World!! I am Handsome!!")
 print("Tristan Bickel IS BEAUTIFUL!!!")
 print("michael tbh im not adding french")
+# pronounced Tahn-seh
+print("tanisi cuzzin!")
