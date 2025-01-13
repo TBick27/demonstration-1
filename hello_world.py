2cb81b525e4e5ee769ef8c9a6a2c2abf4a9054cb
Tristan Bickel <tbickel@academic.rrc.ca>
2025-01-13 10:56:17 -0600

    a new one

--- hello_world.py
+++ hello_world.py
@@ -7,5 +7,5 @@ Date: 2025-01-09
 print("Hello World!! I am Handsome!!")
 print("Tristan Bickel IS BEAUTIFUL!!!")
 print("michael tbh im not adding french")
-# pronounced Tahn-seh
-print("tanisi cuzzin!")
+# pronounced Tahn-seh and Cuh-zin
+print("ᐦᐁᐪᓗ ᐓᕒᐪd ᐊᐣd ᒼᐩ ᒎᓯᐣᐢ")
